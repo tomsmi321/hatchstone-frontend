d625b23b5ace6883bcedd391b1b93b3ca460ed80
Chris Staudinger <chris.j.staudinger@gmail.com>
2019-12-11 09:24:34 +1100

    - cleaned and set up react folder structure. - set up global styles styled component. - wrapped app in react router

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {PrimaryButton} from './uiKit/button'
+import styled from 'styled-components'
+
+function App() {
+  return (
+    <>
+      <PrimaryButton variant="contained">
+        Send message
+      </PrimaryButton>
+    </>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createGlobalStyle } from 'styled-components';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0px;
+    padding: 0px;
+  }
+  html {
+    font-size: 16px;
+  } 
+`
+
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <Router>
+        <App />
+    </Router>
+  </>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
