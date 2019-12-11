69ebf90aede9bbd0d6733ff00e88e08dc4e32fad
Chris Staudinger <chris.j.staudinger@gmail.com>
2019-12-11 11:58:55 +1100

    started rendering landing page to illustrate working with a UI Kit, styled components and modularizing components to have sub-components (keeping code base clean)

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,7 +1,9 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components';
-import { PrimaryButton } from '../../uiKit/button';
+import { PrimaryButton } from '../../uiKit/Button';
 import { ExampleContext } from '../../contexts/ExampleContext';
+import { Background, BackgroundOverlay } from './Background';
+import Typography from '../../uiKit/Typography';
 
 export const LandingPage = () => {
 
@@ -9,8 +11,17 @@ export const LandingPage = () => {
   const exampleContextValue = useContext(ExampleContext);
 
   return(
+    <>
+    <Background>
+      <BackgroundOverlay>
+      <Typography variant="h2" align="center" color="default">
+        Welcome to Hatchstone Client Onboarding
+      </Typography>
+      </BackgroundOverlay>
+    </Background>
     <PrimaryButton variant="contained">
-      `{exampleContextValue.exampleValue}`
+      {exampleContextValue.exampleValue}
     </PrimaryButton>
+    </>
   )
 }
