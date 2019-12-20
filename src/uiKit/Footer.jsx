151424df1f4ba113a6d74dd81cbdefe1358d7f7f
Chris Staudinger <chris.j.staudinger@gmail.com>
2019-12-20 12:10:15 +0800

    Frontend environment (#7)
    * approved button UI Kit component created
    
    * Footer UI kit component added
    
    * Darker Footer UI Kit component added

---
+++ src/uiKit/Footer.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Typography } from '@material-ui/core';
+
+const Wrapper = styled.div`
+  height: 55px;
+  width: 100vw;
+  box-shadow: -1px -2px 0px rgba(0, 0, 0, 0.25);
+  background-color: #FFFFFF;
+  font-style: normal;
+  font-weight: normal;
+  color: #000000;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding-right: 5vw;
+`
+
+const DarkWrapper = styled(Wrapper)`
+  background: #1A1C1F;
+  box-shadow: none;
+  color: #FFFFFF;
+`
+
+export const Footer = () => {
+  return(
+    <Wrapper>
+      <Typography variant="h6">© 2018-2019 Hatchstone Capital Pty Ltd</Typography>
+    </Wrapper>
+    )
+}
+
+export const DarkFooter = () => {
+  return(
+    <DarkWrapper>
+      <Typography variant="h6">© 2018-2019 Hatchstone Capital Pty Ltd</Typography>
+    </DarkWrapper>
+    )
+}
