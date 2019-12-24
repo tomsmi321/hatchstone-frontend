d47c282a72301405851970d0aeda320b17805e5c
Richard Sando <richardsando369@gmail.com>
2019-12-24 13:26:43 +1100

    added documents upload library/ui

---
+++ src/components/DocumentsUpload/UploadFileWrapper.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import FileUpload from "./FileUpload";
+
+const FileUploadWrapper = () => {
+  return (
+    <>
+      <div className="fileUploadWrapper">
+      <h3>Submit Your Documents</h3>
+        <p>Approved Identification</p>
+        <FileUpload documentId={"Approved Identification"} />
+        <p>Proof of Address</p>
+        <FileUpload documentId={"Proof of Address"} />
+        <p>Accounting Statement</p>
+        <FileUpload documentId={"Accounting Statement"} />
+      </div>
+    </>
+  );
+};
+
+export default FileUploadWrapper;
--- src/uiKit/Button.jsx
+++ src/uiKit/Button.jsx
@@ -4,6 +4,7 @@ import styled from "styled-components"
 import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
 
 
+
 // Stylyed component overwritting/adding css to the passed in Button component imported from Material UI
 // '&&&' is to overwrite the css namespace as the most specific selector so that the styles are applied over Material UI's
 const BaseButton = styled(Button)`
