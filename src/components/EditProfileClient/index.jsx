acc18ac7184b6b73af4ca79d881646eb1d266c08
thomassmith <thomassmith@Thomass-MacBook-Pro.local>
2020-01-12 18:49:50 +1100

    setup basic component and route

---
+++ src/components/EditProfileClient/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const EditProfileClientPage = () => {
+    return (
+        <h1>EditProfileClientPage</h1>
+    ) 
+}
+
+export default EditProfileClientPage;
